49ed99bfd1d5437acd50ee8423401be8c1189abf
yuechao.xu <yuechao.xu@appshahe.com>
2022-12-20 14:16:48 +0800

    feat: 初始化项目

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "Api",
+  description: "Metaapp 编辑器的 Api 文档",
+  themeConfig: {
+    algolia: {
+      appId: "8J64VVRP8K",
+      apiKey: "a18e2f4cc5665f6602c5631fd868adfd",
+      indexName: "vitepress",
+    },
+    siteTitle: "Api",
+    nav: [
+      {
+        text: "018 版本",
+        items: [
+          { text: "017 版本", link: "/item-1" },
+          { text: "016 版本", link: "/item-2" },
+          { text: "015 版本", link: "/item-3" },
+        ],
+      },
+      {
+        text: "翻译",
+        items: [
+          { text: "中文", link: "/item-1" },
+          { text: "英文", link: "/item-2" },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        text: "Guide",
+        items: [
+          { text: "Core", link: "/core" },
+          { text: "Game Play", link: "/game-play" },
+          { text: "Events", link: "/events" },
+        ],
+      },
+    ],
+    editLink: {
+      pattern: "https://github.com/vuejs/vitepress/edit/main/docs/:path",
+      text: "到 github 上进行修改",
+    },
+    lastUpdatedText: "Updated Date",
+    docFooter: {
+      prev: "Pagina prior",
+      next: "Proxima pagina",
+    },
+  },
+});
