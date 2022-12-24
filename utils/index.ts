dd0d95b73b1aba6ae925656941feb63b649e2fa7
yuechao.xu <yuechao.xu@appshahe.com>
2022-12-24 15:12:02 +0800

    feat: 添加根据md名字分类的功能

--- utils/index.ts
+++ utils/index.ts
@@ -2,8 +2,8 @@ import { readdirSync } from "fs";
 import * as path from "path";
 import { DefaultTheme } from "vitepress";
 
+/** 根据文件夹生成侧边栏路径 */
 export function pathToSideBar(paths: string[]) {
-  // let obj: Record<string, DefaultTheme.SidebarGroup> = {};
   let sidebar: DefaultTheme.SidebarGroup[] = [];
   paths.forEach((item) => {
     const dirs = readdirSync(path.join(process.cwd(), `./docs/${item}`));
@@ -25,4 +25,69 @@ export function pathToSideBar(paths: string[]) {
   //   });
   // });
   return sidebar;
+}
+
+/** 将目录从类型分类改为根据命名空间分类 */
+export function typeToNamespace(paths: string[]) {
+  interface Info {
+    fullName: string;
+    link: string;
+    name: string;
+    namespace: string;
+  }
+  const targetObjArr = paths
+    .map((p) => {
+      const dirs = readdirSync(path.join(process.cwd(), `./docs/${p}`));
+      return dirs.map((name) => {
+        const info = getMdNameInfo(name);
+        return {
+          ...info,
+          fullName: name,
+          link: `/${p}/${name}`,
+        };
+      });
+    })
+    .flat(2);
+  let namespaceType: Record<string, typeof targetObjArr | undefined> = {};
+  targetObjArr.forEach((obj) => {
+    if (namespaceType[obj.namespace]) {
+      namespaceType[obj.namespace]!.push(obj);
+    } else {
+      namespaceType[obj.namespace] = [obj];
+    }
+  });
+  let sidebar: DefaultTheme.SidebarGroup[] = [];
+  Object.keys(namespaceType).forEach((key) => {
+    const value = namespaceType[key];
+    if (value)
+      sidebar.push({
+        text: key,
+        collapsible: true,
+        collapsed: true,
+        items: value.map((s) => {
+          return {
+            text: s.name,
+            link: s.link,
+          };
+        }),
+      });
+  });
+  return sidebar;
+}
+/**
+ * 根据 md 名字包含的信息
+ * Core.Core.Base.md
+ * ```ts
+ * {
+ *    name: 'Base',
+ *    namespace: 'Core',
+ * }
+ * ```
+ */
+function getMdNameInfo(mdName: string) {
+  const infos = mdName.split(".");
+  return {
+    name: infos.at(-2)!,
+    namespace: infos.at(0)!,
+  };
 }
