ac7170fa2847a3df8b5b76bda73033dd26ef29a4
yuechao.xu <yuechao.xu@appshahe.com>
2023-01-10 18:25:20 +0800

    feat: 添加反馈图标

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -100,7 +100,21 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       // }
     ],
     socialLinks: [
-      { link: 'https://github.com/prodigytech-doc/api-docs', icon: 'github' }
+      { link: 'https://github.com/prodigytech-doc/api-docs', icon: 'github' },
+      {
+        link: 'https://wenjuan.feishu.cn/m/cfm?t=sEJP3cVJZ3Ii-ewdc',
+        icon: {
+          svg: `
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="20" height="20" rx="10" fill="#676D77"/>
+<path d="M14.5 15C15.3284 15 16 14.3284 16 13.5V6.5C16 5.67163 15.3284 5 14.5 5H5.49999C4.67163 5 4 5.67163 4 6.5V13.5C4 14.3284 4.67163 15 5.49999 15H7.74998L10.75 18V15H14.5Z" fill="white"/>
+<path d="M10.0003 13.2857C10.3553 13.2857 10.6431 12.9979 10.6431 12.6429C10.6431 12.2878 10.3553 12 10.0003 12C9.64524 12 9.35742 12.2878 9.35742 12.6429C9.35742 12.9979 9.64524 13.2857 10.0003 13.2857Z" fill="#676D77"/>
+<path d="M10 11V10.4C10 10.1791 10.1822 10.0052 10.3953 9.94698C10.5503 9.90464 10.6983 9.83744 10.8334 9.74721C11.08 9.58238 11.2723 9.34812 11.3858 9.07403C11.4994 8.79994 11.5291 8.49834 11.4712 8.20737C11.4133 7.91639 11.2704 7.64912 11.0607 7.43934C10.8509 7.22956 10.5836 7.0867 10.2926 7.02882C10.0017 6.97094 9.70006 7.00065 9.42597 7.11418C9.15189 7.22771 8.91762 7.41997 8.7528 7.66665C8.58797 7.91332 8.5 8.20333 8.5 8.5" stroke="#676D77" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+        `
+        }
+      }
     ],
     sidebar,
     outline: [2, 3],
