7d9703bcbab5bc6a99f21f2439b86c974e5bcf8e
yuechao.xu <yuechao.xu@appshahe.com>
2023-03-09 18:01:01 +0800

    feat: 替换搜索插件，支持搜索到 20 个

--- viteTheme/theme-default/components/VPAlgoliaSearchBox.vue
+++ viteTheme/theme-default/components/VPAlgoliaSearchBox.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { DefaultTheme } from 'vitepress/theme'
-import docsearch from '@docsearch/js'
+// import docsearch from '@docsearch/js'
+import { docsearch } from '@metaapp/docsearch-react'
 import { onMounted } from 'vue'
 import { useRouter, useRoute, useData } from 'vitepress'
 
@@ -29,14 +30,14 @@ function poll() {
   }, 16)
 }
 
-const docsearch$ = docsearch.default ?? docsearch
+const docsearch$ = docsearch
 type DocSearchProps = Parameters<typeof docsearch$>[0]
 
 function initialize(userOptions: DefaultTheme.AlgoliaSearchOptions) {
   // note: multi-lang search support is removed since the theme
   // doesn't support multiple locales as of now.
   const options = Object.assign<{}, {}, DocSearchProps>({}, userOptions, {
-    container: '#docsearch',
+    container: 'docsearch',
 
     navigator: {
       navigate({ itemUrl }) {
@@ -60,19 +61,19 @@ function initialize(userOptions: DefaultTheme.AlgoliaSearchOptions) {
           url: getRelativePath(item.url)
         })
       })
-    },
-
-    // @ts-expect-error vue-tsc thinks this should return Vue JSX but it returns the required React one
-    hitComponent({ hit, children }) {
-      return {
-        __v: null,
-        type: 'a',
-        ref: undefined,
-        constructor: undefined,
-        key: undefined,
-        props: { href: hit.url, children }
-      }
     }
+
+    // // @ts-expect-error vue-tsc thinks this should return Vue JSX but it returns the required React one
+    // hitComponent({ hit, children }) {
+    //   return {
+    //     __v: null,
+    //     type: 'a',
+    //     ref: undefined,
+    //     constructor: undefined,
+    //     key: undefined,
+    //     props: { href: hit.url, children }
+    //   }
+    // }
   })
 
   docsearch$(options)
