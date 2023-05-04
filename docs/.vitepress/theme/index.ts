c1dbc1a370d8d13016c713790bf3cb2e0e86db85
yuechao.xu <yuechao.xu@appshahe.com>
2023-05-04 10:15:34 +0800

    feat: 添加图片预览功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,23 @@
 import VPTheme from 'metaapp-prodigytech-doc-theme'
-// import VPTheme from 'vitepress/theme'
+import { useRoute } from 'vitepress'
+import { watch, onMounted } from 'vue'
+import mediumZoom, { Zoom } from 'medium-zoom'
+import './index.css'
 
 export default {
-  ...VPTheme
+  ...VPTheme,
+  setup() {
+    const route = useRoute()
+    let zoom: Zoom | undefined
+    watch(route, () => {
+      zoom?.detach()
+      setTimeout(() => {
+        zoom = mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+      })
+    })
+
+    onMounted(() => {
+      zoom = mediumZoom('.main img', { background: 'var(--vp-c-bg)' })
+    })
+  }
 }
